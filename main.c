b66cb6abf3c44607c2ee01cebb55fedc2df09dd2
soo0300 <soo0300@naver.com>
2022-10-18 16:28:28 +0900

    실습_2

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(void) {
-	int flag=1;
-	int y; 
-	while(flag!=0){
-		/* 
-		int y; 
-		y를 지역변수로 선언하면 while 문 밖에서 
-		y 포함한 코드에서 오류남
-		*/
-		y=3;
-		flag=0;
+int main(void){
+	int i;
+	for(i=0; i<5; i++){
+		int temp=1;
+		printf("temp: %d\n",temp);
+		temp++;
+		/*temp 변수가 생성 소멸을 반복*/
 	}
-	y=4;
+	
+	int temp2;
+	/*의도한대로 코드가 돌아가려면 temp2변수를 생존하게 끔*/
+	for(i=0; i<5; i++){
+		printf("temp2: %d\n",temp2);
+		temp2++;
+	}
+	return 0;
+
+	
 }
