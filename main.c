66b10d4af3832cda700dcfde8853907452b94dd4
soo0300 <soo0300@naver.com>
2022-10-18 16:44:44 +0900

    실습_4-1

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int inc(int counter); /*함수 선언!*/
+int counter;
+void set_counter(){
+	counter=20;
+} 
 int main(void){
-	int i=5;
-	printf("함수 호출 전 i = %d\n",i);
-	inc(i);
-	printf("함수호출 후1  i = %d\n",i);
-	i=inc(i);
-	printf("함수호출 후2 i = %d\n",i);
-		
+	printf("counter=%d\n",counter);
+	set_counter();
+	printf("counter=%d",counter);
+	
 	return 0;
-	 
-}
-int inc(int counter){
-	counter++;
-	return counter;
+
+	
 }
